5e52679c78b7af8e776e1eccb70b8f9a666e36dc
Regtables <reghardt7@gmail.com>
2021-10-20 09:49:34 +0200

    added to git

---
+++ order.js
@@ -0,0 +1,22 @@
+const burger = {
+  protein: "",
+  bun: "",
+  lettace: false,
+  tomato: false,
+  pickles: false,
+  onions: false,
+  kimchi: false,
+  cheddar: false,
+  brie: false,
+  feta: false,
+  noCheese: false,
+  tomatoSuace: false,
+  frenchSuace: false,
+  MayoSuace: false,
+  noSuace: false
+
+};
+
+// function getOrder(){
+//   document.querySelectorAll('burger-form-wrapper input');
+// }
