3e705801e84f0c6ffa8024519565264ab54fc34c
Regtables <reghardt7@gmail.com>
2021-10-20 11:55:38 +0200

    added form processing functionality

--- order.js
+++ order.js
@@ -1,22 +1,52 @@
-const burger = {
-  protein: "",
-  bun: "",
-  lettace: false,
-  tomato: false,
-  pickles: false,
-  onions: false,
-  kimchi: false,
-  cheddar: false,
-  brie: false,
-  feta: false,
-  noCheese: false,
-  tomatoSuace: false,
-  frenchSuace: false,
-  MayoSuace: false,
-  noSuace: false
+  const form = document.getElementById("burger-form");
+  const submit = document.getElementById("order");
+  const burger = new FormData();
+ 
+    
 
-};
+  function getFormData(){
 
-// function getOrder(){
-//   document.querySelectorAll('burger-form-wrapper input');
-// }
+    //const burger = new FormData();
+
+    var all = document.querySelectorAll("#burger-form input, #burger-form [type = 'radio'], #burger-form [type = 'checkbox'], #burger-form [type = 'range']");
+  
+ 
+    all.forEach(item => {
+      if(item.checked || item.type === 'range'){
+      //console.log("key: " + item.name, "value: " + item.value)
+      burger.append(item.name, item.value)
+      }
+    })
+
+  } 
+    
+  //   for(let [k, v] of burger){
+  //     console.log(k,v)
+  //   }
+
+  //   for(let v of burger.values()){
+  //     console.log(v)
+  //   }
+  // }
+
+  // submit.addEventListener('click', event =>{
+    
+  //   event.preventDefault();
+  //   getFormData();
+  //   displayOrder();
+
+  // })
+
+  function displayOrder(){
+    for([k,v] of burger){
+      console.log(k,v);
+    }
+  }
+
+  submit.addEventListener('click', event =>{
+    
+    event.preventDefault();
+    getFormData();
+    displayOrder();
+
+  })
